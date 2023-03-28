116eec6bcbf39abea716a229047a47327d4a4e3e
Stanislav Menshykh <smenshykh@s-pro.io>
2023-03-28 16:55:21 +0300

    FIFA-001: [environment] Init

--- src/modules/storage/storage.module.ts
+++ src/modules/storage/storage.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MemoryDbStorageProvider } from './providers/memory-db.provider';
 import { apiConfig, memoryDbStorageConfig } from '../../configs';
+import {MongooseModule} from "@nestjs/mongoose";
 
 @Module({
   providers: [
@@ -21,6 +22,21 @@ import { apiConfig, memoryDbStorageConfig } from '../../configs';
     ConfigModule.forRoot({
       load: [memoryDbStorageConfig],
       cache: apiConfig().isProduction() === true,
+    }),
+    MongooseModule.forRoot('mongodb://locadlhost/demo', {
+      retryAttempts: 3,
+      retryDelay: 10,
+      connectionName: 'TEST',
+      /** The name of the database you want to use. If not provided, Mongoose uses the database name from connection string. */
+      dbName: '',
+      /** username for authentication, equivalent to `options.auth.user`. Maintained for backwards compatibility. */
+      user:  '',
+      /** password for authentication, equivalent to `options.auth.password`. Maintained for backwards compatibility. */
+      pass: '',
+      /** Set to false to disable automatic index creation for all models associated with this connection. */
+      autoIndex: true,
+      /** Set to `true` to make Mongoose automatically call `createCollection()` on every model created on this connection. */
+      autoCreate: true,
     }),
   ],
   exports: [MemoryDbStorageProvider],
--- src/modules/telegram/telegram.module.ts
+++ src/modules/telegram/telegram.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
 import { NezlamniBotService } from './services/nezlamni-bot.service';
 import { apiConfig, telegramConfig } from '../../configs';
 import { StorageModule } from '../storage/storage.module';
@@ -9,21 +8,6 @@ import { StorageModule } from '../storage/storage.module';
   providers: [NezlamniBotService],
   imports: [
     StorageModule,
-    MongooseModule.forRoot('mongodb://locadlhost/demo', {
-      retryAttempts: 3,
-      retryDelay: 10,
-      connectionName: 'TEST',
-      /** The name of the database you want to use. If not provided, Mongoose uses the database name from connection string. */
-      dbName?: string;
-      /** username for authentication, equivalent to `options.auth.user`. Maintained for backwards compatibility. */
-      user?: string;
-      /** password for authentication, equivalent to `options.auth.password`. Maintained for backwards compatibility. */
-      pass?: string;
-      /** Set to false to disable automatic index creation for all models associated with this connection. */
-      autoIndex?: boolean;
-      /** Set to `true` to make Mongoose automatically call `createCollection()` on every model created on this connection. */
-      autoCreate?: boolean;
-    }),
     ConfigModule.forRoot({
       load: [telegramConfig],
       cache: apiConfig().isProduction() === true,
